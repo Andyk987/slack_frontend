83f489a516cfd0ceca4aa9ae08ab3e3725e454a0
JinYoung Kim <jykim@JinYoungui-MacBookAir.local>
2021-06-04 21:46:44 +0900

    fix

---
+++ src/util/get-script.tsx
@@ -0,0 +1,7 @@
+export function getScript(source: string, callback: () => void): void {
+    const el = document.createElement("script");
+    el.addEventListener("load", callback);
+    el.setAttribute("src", source);
+  
+    document.body.appendChild(el);
+  }
