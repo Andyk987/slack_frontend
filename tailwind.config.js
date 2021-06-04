3483351ce2777bdc68762c7f1f30ff2bdbd2d68f
JinYoung Kim <jykim@JinYoungui-MacBookAir.local>
2021-06-04 21:27:21 +0900

    go back

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const plugin = require('tailwindcss/plugin')
 
 module.exports = {
+  important: true,
   purge: [],
   darkMode: "class", // or 'media' or 'class'
   theme: {
@@ -27,6 +28,7 @@ module.exports = {
           more_deep: "#4a154b",
         },
         blue: {
+          lightning: "#4285f4",
           middle: "#1264a3",
           deep: "#1264a3",
         },
@@ -39,12 +41,19 @@ module.exports = {
         thick: "0 0 0 2px #611f69",
         slight_white: "0 0 0 1px #fff",
         thick_white: "0 0 0 2px #fff",
+        loginForm: "0 1px 4px rgb(0 0 0 / 30%)",
+        loginFocus: "0 0 0 1px #1264a3, 0 0 0 5px rgba(29,155,209,.3)",
       },
       transitionProperty: {
         box: 'box-shadow, color, background',
+        border: 'border, box-shadow'
       },
       transitionTimingFunction: {
         cubic: "cubic-bezier(0.165, 0.84, 0.44, 1), cubic-bezier(0.165, 0.84, 0.44, 1), cubic-bezier(0.165, 0.84, 0.44, 1)",
+        
+      },
+      transitionDuration: {
+        "80": "80ms, 80ms",
       },
       gridTemplateColumns: {
         "35-60": "35% 60%",
